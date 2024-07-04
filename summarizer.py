e3be7ada23937e273fe68a38d0833cc2825dc04e
Hemanthkumar2k04 <hemanthkumar2k04@gmail.com>
2024-07-04 15:20:45 +0530

    [Deleted]: output.txt, [change]: summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,8 +1,4 @@
-"""
-with open('output.txt', 'r', encoding='UTF-8') as f:
-    lines = f.read().split('\n\n')
-    for line in lines:
-        print(line+ "\n")"""
+
 from transformers import pipeline
 import time
 import json
@@ -20,15 +16,14 @@ def summarize_and_generate_points(input_text):
     key_points = generate_key_points(summary_text)
     return key_points
 start_time = time.time()
-with open('output.json', 'r', encoding='UTF-8') as f, open('output1.txt', 'w') as f1:
+with open('output.json', 'r', encoding='UTF-8') as f:
     datas = json.load(f)
     for data in datas:
         key_points = summarize_and_generate_points(data['short_description'])
         print("\nGenerated Key Points:")
         for i, point in enumerate(key_points, 1):
             print(f"{i}. {point.strip()}.")
-            f1.write(f"{i}. {point.strip()}\n")
-        f1.write('\n')
+
 
 end_time = time.time()
 print(f"Time taken to summarize {len(datas)} products: {end_time - start_time:.2f} seconds")
